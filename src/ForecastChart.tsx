3d7b78c088abfa6e11329eb8d45f03943543a7ec
Tejen Shrestha <shrestha.tejen@gmail.com>
2026-01-21 11:49:27 -0700

    combine data point label and shape

--- src/ForecastChart.tsx
+++ src/ForecastChart.tsx
@@ -33,8 +33,13 @@ export default function ForecastChart({
             return
         }
 
-        const { paddingRight } = getComputedStyle(containerRef)
-        const margin = { top: 20, left: 50, right: parseInt(paddingRight), bottom: 20 }
+        const { paddingLeft, paddingRight, paddingTop, paddingBottom } = getComputedStyle(containerRef)
+        const margin = {
+            top: parseInt(paddingTop),
+            left: parseInt(paddingLeft),
+            right: parseInt(paddingRight),
+            bottom: parseInt(paddingBottom)
+        }
         const width = containerRef.clientWidth - margin.left - margin.right
         const height = periods.length * 40
         const curve = curveCatmullRom.alpha(1)
@@ -42,9 +47,7 @@ export default function ForecastChart({
         const xMin = Math.min(...periods.map((p) => getX(p)))
         const xMax = Math.max(...periods.map((p) => getX(p)))
         const relativeMinX = xMin - (xMax - xMin)
-        const dataPointCircleRadius = 6
         const timeLabelPadding = 10
-        const dataPointLabelPadding = dataPointCircleRadius + dataPointCircleRadius / 2
 
         // data-value to color scale
         const tempColorScale = scaleLinear<string>().domain(colorDomain).range(colorRange).clamp(true)
@@ -84,23 +87,28 @@ export default function ForecastChart({
         // Use the maxTimeLabelWidth to determine how much the chart area needs to shift right
         svg.attr('transform', `translate(${maxTimeLabelWidth + timeLabelPadding},0)`)
 
-        // Add data point labels on the right (colored by data point)
-        const dataPointLabels = svg
-            .selectAll('.x-label')
+        const dataPointLabelGroup = svg
+            .selectAll('.data-point-label-group')
             .data(periods)
             .enter()
+            .append('g')
+            .attr('class', 'data-point-label-group')
+
+        const dataPointLabelBg = dataPointLabelGroup.append('rect').attr('class', 'data-point-label-bg')
+
+        // Add data point labels on the right (colored by data point)
+        const dataPointLabels = dataPointLabelGroup
             .append('text')
-            .attr('class', 'x-label')
-            .attr('dy', '0.35em')
-            .attr('text-anchor', 'start')
-            .attr('font-size', '14px')
-            // .attr('font-weight', 'bold')
+            .attr('class', 'data-point-label')
+            .attr('font-size', '0.875rem')
             .attr('fill', 'var(--bs-body-color)')
-            // .attr('fill-opacity', '0.75')
             .text((d) => `${getXLabel(d)}`)
 
-        const dataPoinLabelWidths = dataPointLabels.nodes().map((n) => n.scrollWidth)
-        const maxDataPointLabelWidth = Math.max(...dataPoinLabelWidths)
+        const dataPointLabelDim = dataPointLabels.nodes().map((n) => ({ width: n.scrollWidth, height: n.scrollHeight }))
+        const maxDataPointLabelWidth = Math.max(...dataPointLabelDim.map((d) => d.width))
+        const dataPointLabelHeight = dataPointLabelDim[0].height
+        const dataPointLabelBgWidth = maxDataPointLabelWidth * 1.618
+        const dataPointLabelBgStrokeWidth = 2
 
         // X scale for data point (horizontal)
         const xScale = scaleLinear()
@@ -112,15 +120,27 @@ export default function ForecastChart({
                     margin.right -
                     maxTimeLabelWidth -
                     timeLabelPadding -
-                    dataPointLabelPadding -
-                    maxDataPointLabelWidth -
-                    dataPointCircleRadius
+                    dataPointLabelBgWidth / 2 -
+                    dataPointLabelBgStrokeWidth
             ])
 
-        svg.selectAll('text.x-label')
-            .data(periods)
-            .attr('x', (d) => xScale(getX(d)) + dataPointLabelPadding)
-            .attr('y', (_, i) => yScale(i) ?? 0)
+        dataPointLabelBg
+            .attr('height', dataPointLabelHeight)
+            .attr('width', dataPointLabelBgWidth)
+            .attr('rx', dataPointLabelHeight / 2)
+            .attr('fill', (d) => tempColorScale(getColorValue(d)).replace('0.6)', '1)'))
+            .attr('stroke', 'white')
+            .attr('stroke-width', dataPointLabelBgStrokeWidth)
+
+        dataPointLabelGroup.attr(
+            'transform',
+            (d, i) =>
+                `translate(${xScale(getX(d)) - dataPointLabelBgWidth / 2}, ${(yScale(i) ?? 0) - dataPointLabelHeight / 2})`
+        )
+        dataPointLabels
+            .attr('dy', dataPointLabelHeight * 0.5 + dataPointLabelHeight * 0.25)
+            .attr('dx', maxDataPointLabelWidth * 0.25 + dataPointLabelBgStrokeWidth)
+            .attr('fill', 'white')
 
         // Create the area generator for vertical orientation
         const areaGenerator = area<Period>()
@@ -129,38 +149,22 @@ export default function ForecastChart({
             .y((_, i) => yScale(i) ?? 0)
             .curve(curve)
 
+        const lineGenerator = line<Period>()
+            .x((d) => xScale(getX(d)))
+            .y((_, i) => yScale(i) ?? 0)
+            .curve(curve)
+
         // Create defs for gradients and filters
         const defs = svg.append('defs')
 
         // Create vertical gradient based on data point at each point
-        const gradient = defs
+        const areaGradient = defs
             .append('linearGradient')
             .attr('id', 'temp-gradient')
             .attr('x1', '0%')
             .attr('x2', '0%')
             .attr('y1', '0%')
             .attr('y2', '100%')
-
-        // Add gradient stops for each data point based on its data point
-        periods.forEach((period, i) => {
-            const offset = (i / (periods.length - 1)) * 100
-            gradient
-                .append('stop')
-                .attr('offset', `${offset}%`)
-                .attr('stop-color', tempColorScale(getColorValue(period)))
-        })
-
-        // Draw the area
-        svg.append('path')
-            .datum(periods)
-            .attr('fill', 'url(#temp-gradient)')
-            .attr('fill-opacity', 0.4)
-            .attr('d', areaGenerator)
-
-        const lineGenerator = line<Period>()
-            .x((d) => xScale(getX(d)))
-            .y((_, i) => yScale(i) ?? 0)
-            .curve(curve)
 
         // Create gradient for the line stroke
         const lineGradient = defs
@@ -171,13 +175,20 @@ export default function ForecastChart({
             .attr('y1', '0%')
             .attr('y2', '100%')
 
+        // Add gradient stops for each data point based on its data point
         periods.forEach((period, i) => {
             const offset = (i / (periods.length - 1)) * 100
-            lineGradient
-                .append('stop')
-                .attr('offset', `${offset}%`)
-                .attr('stop-color', tempColorScale(getColorValue(period)).replace('0.6)', '1)'))
+            const color = tempColorScale(getColorValue(period))
+            areaGradient.append('stop').attr('offset', `${offset}%`).attr('stop-color', color)
+            lineGradient.append('stop').attr('offset', `${offset}%`).attr('stop-color', color.replace('0.6)', '1)'))
         })
+
+        // Draw the area
+        svg.append('path')
+            .datum(periods)
+            .attr('fill', 'url(#temp-gradient)')
+            .attr('fill-opacity', 0.4)
+            .attr('d', areaGenerator)
 
         svg.append('path')
             .datum(periods)
@@ -232,23 +243,10 @@ export default function ForecastChart({
             .attr('x2', (d) => xScale(getX(d)))
             .attr('y2', (_, i) => yScale(i) ?? 0)
             .attr('stroke', (d) => tempColorScale(getColorValue(d)).replace('0.6)', '1)'))
-            // .attr('stroke-opacity', '0.5')
             .attr('stroke-width', 1)
             .attr('stroke-dasharray', '2,2')
 
-        // Add dots at data points with data point-based colors
-        svg.selectAll('.data-point')
-            .data(periods)
-            .enter()
-            .append('circle')
-            .attr('class', 'data-point')
-            .attr('cx', (d) => xScale(getX(d)))
-            .attr('cy', (_, i) => yScale(i) ?? 0)
-            .attr('r', dataPointCircleRadius)
-            .attr('fill', (d) => tempColorScale(getColorValue(d)).replace('0.6)', '1)'))
-            .attr('stroke', 'white')
-            .attr('stroke-width', 2)
-
+        dataPointLabelGroup.raise()
         dataPointLabels.raise()
 
         onCleanup(() => {
