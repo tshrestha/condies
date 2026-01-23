9f770ccffaa3eda28132fc6d83f7559f9ff861f8
Tejen Shrestha <shrestha.tejen@gmail.com>
2026-01-23 13:07:09 -0700

    fix wind speed

--- src/WindSpeed.tsx
+++ src/WindSpeed.tsx
@@ -4,11 +4,11 @@ import type { QuantitativeValue } from "./lib/nws.ts"
 
 export default function WindSpeed({ windSpeed }: { windSpeed: QuantitativeValue }) {
     return (
-        <Switch>
-            <Match when={typeof windSpeed.minValue !== "undefined" && typeof windSpeed.maxValue !== "undefined"}>
+        <Switch fallback={<small>0 mph</small>}>
+            <Match when={windSpeed && typeof windSpeed.minValue !== "undefined" && typeof windSpeed.maxValue !== "undefined"}>
                 <small>{`${windSpeed.minValue} - ${windSpeed.maxValue} mph`}</small>
             </Match>
-            <Match when={typeof windSpeed.value !== "undefined"}>
+            <Match when={windSpeed && typeof windSpeed.value !== "undefined"}>
                 <small>{`${windSpeed.value} mph`}</small>
             </Match>
         </Switch>
